6884d40492a22aa81a0d16e4126087e18d133021
joesephorekhov <josephorekhov@gmail.com>
2021-07-16 15:37:57 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+   name : `joe`,
+   street : `Broknn Dreams`
+};
+
+//The curly brackets make a new object and modifies that one. not the original.
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, {[key] : value});
+};
+const newEmployee = updateEmployeeWithKeyAndValue(employee, key, value);
+
+//There are no curly brackets thus being distructive.
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign(employee, {[key] : value});
+};
+
+//A clone of "employee" object called "newEmployee". And modifly the clone "newEmployee"
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+};
+
+
+ /* const newEmployee = {...employee};
+    newEmployee.name =`$[value]`;
+    return newEmployee.name;*/
